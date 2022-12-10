dbea887a8ef8b21d6c6c7a1a8811bf62a2b8ff6e
ArtemShchuchev <shuaa@rambler.ru>
2022-12-10 22:47:46 +0700

    Добавьте файлы проекта.

---
+++ Task1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream> // консоль - cout
+#include <vector>
+#include <algorithm>    // std::for_each
+
+void print(const std::vector<int>& v, const std::string& str)
+{
+    std::cout << str << " данные: ";
+    for (const auto& val : v) std::cout << val << " ";
+    std::cout << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");   // задаём русский текст
+    system("chcp 1251");            // настраиваем кодировку консоли
+    std::system("cls");
+
+    std::cout   << "Задача 1. Лямбда-функция\n"
+                << "------------------------\n" << std::endl;
+
+    std::vector<int> vect{4, 7, 9, 14, 12};
+
+    print(vect, "Входные");
+
+    // без чтения подсказки, а потом вопросов к Гугл, решить это не возможно
+    // т.к. только из подсказки и узнал о существовании функции std::for_each,
+    // а как с ней работать из поиска в сети ((
+    std::for_each( std::begin(vect), std::end(vect),
+        [](int & value) { if (value % 2) value *= 3; } );
+
+    print(vect, "Выходные");
+
+    return 0;
+}
